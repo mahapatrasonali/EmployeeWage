fbfee32844921cabfa7480540b4b28294630a944
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-15 19:24:23 +0530

    Switch Case

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -8,30 +8,29 @@ namespace EmployeeWageProblem
 {
     public class EmployeeCheck
     {
+       public const int IS_PART_TIME = 1;
+       public const int IS_FULL_TIME = 2;
+       public const int EMP_RATE_PER_HOUR = 20;
         public static void CheckAttendence()
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+           
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
 
-            if(empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                
-               empHrs = 4;
-            }
-            else if(empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0; 
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage: " + empWage);

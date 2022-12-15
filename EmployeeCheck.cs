8a34459dd1c32b5c9c87c6582510b0df0d60eaa2
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-15 18:31:09 +0530

    Add Part time employee

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -10,18 +10,22 @@ namespace EmployeeWageProblem
     {
         public static void CheckAttendence()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
 
-            if(empCheck == IS_FULL_TIME)
+            if(empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                
+               empHrs = 4;
+            }
+            else if(empCheck == IS_FULL_TIME)
+            {
                 empHrs = 8;
             }
             else
@@ -30,6 +34,7 @@ namespace EmployeeWageProblem
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }

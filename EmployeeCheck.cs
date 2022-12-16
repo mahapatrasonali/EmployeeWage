afe27fb9cf5866c40dd1bc190a19d1d22d476bf8
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-16 09:36:46 +0530

    UC_6

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -13,16 +13,18 @@ namespace EmployeeWageProblem
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
         public static void CheckAttendence()
         {
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(3);
 
@@ -40,14 +42,14 @@ namespace EmployeeWageProblem
                         empHrs = 0;
                         break;
                 }
-
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empCheck;
-
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
 
 
-                Console.WriteLine("EmpWage: " + empWage);
+
+                
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("totalEmpwage:" + totalEmpWage);
         }
     }
